f574b9ff5f006b23f1dade3a2c487a3ded7cb859
Guanjie Chen <chgjie7053@gmail.com>
2014-09-05 17:41:47 -0400

    1

--- src/MasterNode.java
+++ src/MasterNode.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.io.*;
 /**
  * Created by wenhanl on 14-9-4.
  */
@@ -9,5 +9,39 @@ public class MasterNode {
     MasterNode(){
         slaveList = new ArrayList<>();
         // Listen to slave connections
+        BufferedReader buffInput = new BufferedReader(new InputStreamReader(System.in));
+        String cmdInput = "";
+        while(true)
+        {
+            System.out.print("--> ");
+            try {
+                cmdInput = buffInput.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String args[]=cmdInput.split(" ");
+            if(args.length==0)
+                continue;
+            else if(args.length==1){
+                if(args[0].equals("list"))
+                    printslaveList();
+            }
+            else if(args.length==2){
+                if(args[0].equals("terminate")){
+                    /* TODO */
+                }
+            }
+            else if(args.length==3){
+                if(args[0].equals("run")){
+                    /* TODO */
+                }
+                else if(args[0].equals("migrate")){
+                    /* TODO */
+                }
+            }
+        }
+    }
+    void printslaveList(){
+        /* TODO */
     }
 }

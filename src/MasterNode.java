40a79a8d1566f162fe0e5573b8496fb92710e6a8
wenhanl <lvwenhan417@gmail.com>
2014-09-05 17:22:47 -0400

    Socket connection added

--- src/MasterNode.java
+++ src/MasterNode.java
@@ -1,3 +1,8 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 /**
@@ -5,9 +10,28 @@ import java.util.ArrayList;
  */
 public class MasterNode {
     private ArrayList<NodeInfo> slaveList;
+    private ServerSocket socketServer;
+    public static final int PORT = 15640;
+
 
     MasterNode(){
         slaveList = new ArrayList<>();
-        // Listen to slave connections
+    }
+
+    public void start(){
+        // Start socket sever Listen to slave connections
+        try {
+            socketServer = new ServerSocket(PORT);
+            while(true){
+                Socket sock = socketServer.accept();
+                DataInputStream input = new DataInputStream(sock.getInputStream());
+                DataOutputStream output = new DataOutputStream(sock.getOutputStream());
+                NodeInfo slave = new NodeInfo(sock, input, output);
+                slaveList.add(slave);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
